d9d490b6c94776612dceb7f84d2f91a219ff8808
szhbest <51316125+szhbest@users.noreply.github.com>
2020-03-03 13:57:35 +0800

    Update scatter_chart.py

--- Week4 Data Visualization/scatter_chart.py
+++ Week4 Data Visualization/scatter_chart.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     virginica = iris_df.query('species == "virginica"')
     versicolor = iris_df.query('species == "versicolor"')
 
+
+    # create two figures individually
     ax1 = setosa.plot.scatter(x='sepal_length', y='sepal_width', label='setosa')
     ax1 = virginica.plot.scatter(x='sepal_length', y='sepal_width', label='virginica', c='r', ax=ax1)
     ax1 = versicolor.plot.scatter(x='sepal_length', y='sepal_width', label='versicolor', c='g', ax=ax1)
@@ -22,4 +24,18 @@ if __name__ == '__main__':
     ax2 = versicolor.plot.scatter(x='petal_length', y='petal_width', label='versicolor', color='g', ax=ax2)
     ax2 = virginica.plot.scatter(x='petal_length', y='petal_width', label='virginica', color='r', ax=ax2)
     
+    
+    # create the subplots with matplotlib, in just one figure
+    fig, axes = plt.subplots(nrows=2, ncols=1)
+
+    ax1 = setosa.plot.scatter(x='sepal_length', y='sepal_width', label='setosa', ax=axes[0])
+    ax1 = virginica.plot.scatter(x='sepal_length', y='sepal_width', label='virginica', c='r', ax=ax1)
+    ax1 = versicolor.plot.scatter(x='sepal_length', y='sepal_width', label='versicolor', c='g', ax=ax1)
+
+    ax2 = setosa.plot.scatter(x='petal_length', y='petal_width', label='setosa', ax=axes[1])
+    ax2 = versicolor.plot.scatter(x='petal_length', y='petal_width', label='versicolor', color='g', ax=ax2)
+    ax2 = virginica.plot.scatter(x='petal_length', y='petal_width', label='virginica', color='r', ax=ax2)
+    
     plt.show()
+    
+    
